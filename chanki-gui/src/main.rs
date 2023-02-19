adec010a3a83795e764a8e0e387bf0bd1950271d
Brent Westbrook <brentrwestbrook@gmail.com>
2023-02-19 15:50:10 -0600

    add `main` and `add` views

--- chanki-gui/src/main.rs
+++ chanki-gui/src/main.rs
@@ -1,116 +1,94 @@
 mod app {
-    /// We derive Deserialize/Serialize so we can persist app state on shutdown.
-    pub struct TemplateApp {
-        // Example stuff:
-        label: String,
-        value: f32,
+    use eframe::CreationContext;
+    use egui::{Button, TextEdit, Ui, Visuals};
+
+    pub struct App {
+        view: fn(app: &mut App, ui: &mut Ui),
+        pgn: String,
+        half_move: String,
+        answer: String,
     }
 
-    impl Default for TemplateApp {
+    impl Default for App {
         fn default() -> Self {
             Self {
-                // Example stuff:
-                label: "Hello World!".to_owned(),
-                value: 2.7,
+                view: Self::main_view,
+                pgn: String::new(),
+                half_move: String::new(),
+                answer: String::new(),
             }
         }
     }
 
-    impl TemplateApp {
+    impl App {
         /// Called once before the first frame.
-        pub fn new() -> Self {
-            // This is also where you can customize the look and feel of egui
-            // using `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
-
+        pub fn new(cc: &CreationContext<'_>) -> Self {
+            cc.egui_ctx.set_visuals(Visuals::light());
             Default::default()
+        }
+
+        fn main_view(&mut self, ui: &mut egui::Ui) {
+            if ui.add(Button::new("Add card")).clicked() {
+                self.view = Self::add_view;
+            }
+            ui.add(Button::new("Edit card"));
+            ui.add(Button::new("Review"));
+        }
+
+        fn add_view(&mut self, ui: &mut egui::Ui) {
+            let pgn_input = TextEdit::multiline(&mut self.pgn)
+                .desired_rows(12)
+                .hint_text("Enter PGN");
+            pgn_input.show(ui);
+
+            let half_move = TextEdit::singleline(&mut self.half_move)
+                .hint_text("Half Move");
+            half_move.show(ui);
+
+            let answer =
+                TextEdit::singleline(&mut self.answer).hint_text("Answer");
+            answer.show(ui);
+
+            ui.add(Button::new("Add"));
+            if ui.add(Button::new("Done")).clicked() {
+                self.view = Self::main_view;
+            }
         }
     }
 
-    impl eframe::App for TemplateApp {
+    impl eframe::App for App {
         /// Called each time the UI needs repainting, which may be many times
         /// per second. Put your widgets into a `SidePanel`, `TopPanel`,
         /// `CentralPanel`, `Window` or `Area`.
-        fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-            let Self { label, value } = self;
-
-            // Examples of how to create different panels and windows. Pick
-            // whichever suits you. Tip: a good default choice is to just keep
-            // the `CentralPanel`. For inspiration and more examples, go to
-            // https://emilk.github.io/egui
-
+        fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
             #[cfg(not(target_arch = "wasm32"))] // no File->Quit on web pages!
             egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
                 // The top panel is often a good place for a menu bar:
                 egui::menu::bar(ui, |ui| {
                     ui.menu_button("File", |ui| {
                         if ui.button("Quit").clicked() {
-                            _frame.close();
+                            frame.close();
                         }
                     });
                 });
             });
 
-            egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
-
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
-            });
-
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
-            }
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                ui.horizontal(|ui| {
-                    ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
-                    ui.hyperlink_to(
-                        "eframe",
-                        "https://github.com/emilk/egui/tree/master/crates/eframe",
-                    );
-                    ui.label(".");
-                });
-            });
-        });
-
             egui::CentralPanel::default().show(ctx, |ui| {
-                // The central panel the region left after adding TopPanel's and SidePanel's
-
-                ui.heading("eframe template");
-                ui.hyperlink("https://github.com/emilk/eframe_template");
-                ui.add(egui::github_link_file!(
-                    "https://github.com/emilk/eframe_template/blob/master/",
-                    "Source code."
-                ));
-                egui::warn_if_debug_build(ui);
+                ui.vertical_centered(|ui| {
+                    (self.view)(self, ui);
+                })
             });
-
-            if false {
-                egui::Window::new("Window").show(ctx, |ui| {
-                    ui.label("Windows can be moved by dragging them.");
-                    ui.label("They are automatically sized based on contents.");
-                    ui.label(
-                        "You can turn on resizing and scrolling if you like.",
-                    );
-                    ui.label(
-                        "You would normally choose either panels OR windows.",
-                    );
-                });
-            }
         }
     }
 }
 
+const APP_NAME: &str = "chanki";
+
 fn main() -> eframe::Result<()> {
-    let native_options = eframe::NativeOptions::default();
+    let options = eframe::NativeOptions::default();
     eframe::run_native(
-        "eframe template",
-        native_options,
-        Box::new(|_| Box::new(app::TemplateApp::new())),
+        APP_NAME,
+        options,
+        Box::new(|cc| Box::new(app::App::new(cc))),
     )
 }
